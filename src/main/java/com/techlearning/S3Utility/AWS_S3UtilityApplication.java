bd9729f01c486640f50c82394ab1b19e203b5fcd
lipibanerjee <lbanerjee805@gmail.com>
2025-08-28 19:39:28 +0530

    Add AWS S3 utility for file upload and download

---
+++ src/main/java/com/techlearning/S3Utility/AWS_S3UtilityApplication.java
@@ -0,0 +1,15 @@
+package com.techlearning.S3Utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWS_S3UtilityApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(AWS_S3UtilityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/techlearning/S3Utility/service/S3Service.java
@@ -0,0 +1,44 @@
+package com.techlearning.S3Utility.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+public class S3Service {
+
+    @Autowired
+    private S3Client s3Client;
+
+    @Value("${aws.bucket.name}")
+    private String bucketName;
+
+    public void uploadFile(MultipartFile file) throws IOException {
+//        String key = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(file.getOriginalFilename())
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putRequest, RequestBody.fromBytes(file.getBytes()));
+
+    }
+
+    public byte[] downloadFile(String key) {
+        ResponseBytes<GetObjectResponse> objectAsBytes = s3Client.getObjectAsBytes(GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build());
+        return objectAsBytes.asByteArray();
+    }
+}
